12ee8be1cdf68559b359707e5a32dddbcd539bc6
German Feskov <g.feskov@yandex.ru>
2026-01-12 12:23:50 +0300

    Добавлена тула для подсчёта облаков распределения

--- cmake/Project.cmake
+++ cmake/Project.cmake
@@ -11,3 +11,9 @@ add_library(harq STATIC ${HARQ_SOURCES})
 target_include_directories(harq PUBLIC
     ${CMAKE_CURRENT_LIST_DIR}/../include
 )
+
+add_executable(bpsk_passband_cloud
+    ${CMAKE_CURRENT_LIST_DIR}/../tools/bpsk_passband_cloud.cpp
+)
+
+target_link_libraries(bpsk_passband_cloud PRIVATE harq)
